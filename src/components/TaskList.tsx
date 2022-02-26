6aaa230f08fca8bb409556422a7b58239dcf4c68
Leonardo Vogel <leeo.vog13@gmail.com>
2022-02-26 05:55:37 -0300

    feat: add function create new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,16 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.floor(Math.random() * 9_000),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks((oldTasks) => [...oldTasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
