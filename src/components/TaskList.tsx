470d64e9df2944cc684d562299ae52aeb467d624
Leonardo Vogel <leeo.vog13@gmail.com>
2022-02-26 06:09:08 -0300

    feat: add function change task completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -28,7 +28,12 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTaskList = tasks.map((task) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
+
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
