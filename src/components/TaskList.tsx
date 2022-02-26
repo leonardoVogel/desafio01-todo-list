4c5ce68dfe8ca73ad46dfaa91378d6cc8b28b55b
Leonardo Vogel <leeo.vog13@gmail.com>
2022-02-26 06:03:58 -0300

    feat: add function remove task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -32,7 +32,9 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTaskList = tasks.filter((task) => task.id !== id);
+
+    setTasks(filteredTaskList)
   }
 
   return (
